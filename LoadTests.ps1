8da78e97298a660673fb0e55c40239226754df46
Robby Millsap <valleysap@hotmail.com>
2019-07-31 17:03:12 -0500

    Updated powershell

--- LoadTests.ps1
+++ LoadTests.ps1
@@ -1,68 +1,49 @@
-Write-Host "Load test failed"
-exit 1
+Add-Type -AssemblyName System.Web
 
-# Add-Type -AssemblyName System.Web
+$rpsThreshold = 2
 
-# $token = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$(PersonalAccessToken)"))
+$token = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("username:gtn54zvllxsaajfa4h2irwldti4tinmxucbctop3zzvp27hzq4cq"))
 
-# $headers =  @{Authorization=("Basic {0}" -f $token)}
-# $headers.Add("Accept", "application/json")
-# $headers.Add("Content-Type", "application/json")
+$headers =  @{Authorization=("Basic $token")}
+$headers.Add("Accept", "application/json")
+$headers.Add("Content-Type", "application/json")
 
-# $testRunNameParameter =  [System.Web.HttpUtility]::UrlEncode("$(TestRunName)")
+$response = Invoke-RestMethod -Method GET `
+                                  -Headers $headers `
+                                  -Uri "https://iamsap.vsclt.visualstudio.com/_apis/clt/testruns?status=completed&runType=*&api-version=5.0" 
 
-# #at the time of this script, there was no server side ordering of the test runs so need to query all to get the two most recent.
-# $response = Invoke-RestMethod -Method GET `
-#                                   -Headers $headers `
-#                                   -Uri "https://trekbikes.vsclt.visualstudio.com/_apis/clt/testruns?status=completed&name=$testRunNameParameter&runType=*&fromDate=2018-01-01&api-version=5.0-preview.1" 
+$testRunRPSCounter = $testRunCounters.value | Where-Object -Property "counterName" -EQ "Requests/Sec"
+$testRun = $response | Sort-Object -Property RunNumber -Descending | Select-Object -First 1
 
-# $twoMostRecentRuns = $response | Sort-Object -Property RunNumber -Descending | Select-Object -First 2
 
-# #if we don't have two runs just exit
-# if ($twoMostRecentRuns.Count -lt 2) {
-#     Write-Host "Not enough test runs to run comparison"
-#     exit 0
-# }
+$testRunId = $testRun.id
 
-# #query Throughput counter results for each run
-# $testRunAverageRPS = @()
-# foreach ($testRun in $twoMostRecentRuns){
-#     $testRunId = $testRun.id
-#     $testRunCounters = Invoke-RestMethod -Method GET `
-#                                           -Headers $headers `
-#                                           -Uri "https://trekbikes.vsclt.visualstudio.com/_apis/clt/testruns/$testRunId/CounterInstances?groupNames=Throughput&api-version=5.0-preview.1"
-#     #get requestPersecond counter id
-#     $testRunRPSCounter = $testRunCounters.value | Where-Object -Property "counterName" -EQ "Requests/Sec"
+$testRunCounters = Invoke-RestMethod -Method GET `
+                                         -Headers $headers `
+                                         -Uri "https://iamsap.vsclt.visualstudio.com/_apis/clt/testruns/$testRunId/CounterInstances?groupNames=Throughput&api-version=5.0"
 
-#     #query results for the Requests/Second counter
+$testRunRPSCounter = $testRunCounters.value | Where-Object -Property "counterName" -EQ "Requests/Sec"
 
-#     $rpsCounterId = $testRunRPSCounter.counterInstanceId
-#     $rpsResult = Invoke-RestMethod -Method POST `
-#                                    -Headers $headers `
-#                                    -Uri "https://trekbikes.vsclt.visualstudio.com/_apis/clt/testruns/$testRunId/countersamples?api-version=5.0-preview.1" `
-#                                    -Body (@{ count = 1
-#                                             value=@(@{counterInstanceId=$rpsCounterId})
-#                                           } | ConvertTo-Json)
 
-#     $rpsResultValues = $rpsResult.values[0].values
-    
-#     #get rps values not equal to zero and average 
-#     $rpsMeasure = $rpsResultValues | Where-Object -Property "computedValue" -NE 0 | Measure-Object -Property "computedValue" -Average
+$rpsCounterId = $testRunRPSCounter.counterInstanceId
+$rpsResult = Invoke-RestMethod -Method POST `
+                               -Headers $headers `
+                               -Uri "https://iamsap.vsclt.visualstudio.com/_apis/clt/testruns/$testRunId/countersamples?api-version=5.0" `
+                               -Body (@{ count = 1
+                                   value=@(@{counterInstanceId=$rpsCounterId})
+                                    } | ConvertTo-Json)
 
-#     if($rpsMeasure){ $averageRPS = $rpsMeasure.Average } else { $averageRPS = 0 }
+$rpsResultValues = $rpsResult.values[0].values
 
-#     Write-Host "Average RPS is: $averageRPS for test run:"$testRun.runNumber  
-#     $testRunAverageRPS+=$averageRPS
-# }
-# #if most recent test run had an average rps < threshold * second most recent run then return false
-# if($testRunAverageRPS[0] -gt $testRunAverageRPS[1]){
-#     # if latest test run did more RPS were gtg
-#     Write-Host "Latest test run out performed previous"
-#     exit 0
-# }
+$rpsMeasure = $rpsResultValues | Where-Object -Property "computedValue" -NE 0 | Measure-Object -Property "computedValue" -Average
 
-# if(($testRunAverageRPS[1]-$testRunAverageRPS[0]) -gt ($testRunAverageRPS[1] * $performanceThreshold)){
-#     Write-Host "Perfomance has dropped more than performance threshold"
-#     exit 1
-# }
-# Write-Host "Performance of latest run is within the threshold of the previous run"
+if($rpsMeasure){ $averageRPS = $rpsMeasure.Average } else { $averageRPS = 0 }
+
+if($averageRPS -le $rpsThreshold )
+{
+    Write-Host "averageRPS is less or equal than $rpsThreshold"
+    exit 1
+}else {
+    Write-Host "averageRPS is greater than $rpsThreshold"
+    exit 0
+}
